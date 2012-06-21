3f83c693fbcd593a7fb5ddc820af48c9963c8067
niwaringo <niwaringo@gmail.com>
2012-06-21 12:58:39 +0900

    add select option

--- jquery.optremover/jquery.optremover.js
+++ jquery.optremover/jquery.optremover.js
@@ -1,23 +1,24 @@
 (function ($) {
+    'use strict';
     $.fn["optRemover"] = function(options) {
-        var $obj = $(this);
+        var $obj = $(this),
 
         /*
         *  設定ファイル
         *  @type input || conf
         *  @text array
         */
-        var settings = $.extend({
+        settings = $.extend({
             'type': 'input',
             'txt': []
-        }, options);
+        }, options),
 
-        var txtLen = settings.txt.length;
+        txtLen = settings.txt.length,
 
-        var hideAct = {
+        hideAct = {
             'input': function($obj, index) {
-                var $obj_br = $obj.next('br');
-                var this_nextSib = $obj[0].nextSibling;
+                var $obj_br = $obj.next('br'),
+                this_nextSib = $obj[0].nextSibling;
 
                 if (this_nextSib.nodeValue.match(settings.txt[index])) {
                     $obj.hide();
@@ -25,25 +26,33 @@
                     $obj_br.hide();
                 }
             },
+            'select': function($obj, index) {
+                var text = $obj.text();
+
+                if (text === settings.txt[index]) {
+                    $obj.hide();
+                }
+            },
             'conf': function($obj, index) {
-                var $parent = $obj.parent();
-                var old_html = $parent.html();
+                var $parent = $obj.parent(),
+                old_html = $parent.html(),
+                new_html = '';
 
                 if (!!old_html && old_html.match(settings.txt[index])) {
-                    var new_html = old_html.replace(settings.txt[index] + '<br>', '');
+                    new_html = old_html.replace(settings.txt[index] + '<br>', '');
                     $parent.html(new_html);
                 }
             }
         };
 
         $obj.each(function() {
-            var $this = $(this);
+            var $this = $(this), i;
 
-            for (var i = 0; i < txtLen; i++) {
+            for (i = 0; i < txtLen; i++) {
                 hideAct[settings.type]($this, i);
             }
         });
 
         return this;
     };
-})(jQuery);
+}(window.jQuery));
