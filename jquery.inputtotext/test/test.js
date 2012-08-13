f55bf59a1182801b06bb591beac92bca85ca797a
niwaringo <niwaringo@gmail.com>
2012-08-13 18:14:50 +0900

    add test

--- jquery.inputtotext/test/test.js
+++ jquery.inputtotext/test/test.js
@@ -18,7 +18,30 @@
     });
 
     test('input radio testing', function() {
-        var $test_target = $('#ss_admin_visitordata_d__1__d_sub_email_valid_row input'),
-        $text_target;
+        var $test_target = $('#ss_admin_visitordata_d__1__d_sub_email_valid_row input');
+        $test_target.inputToText();
+
+        strictEqual(getTargetText($test_target), '有効 ');
+    });
+
+    test('input checkbox testing - single check -', function() {
+        var $test_target = $('#ss_admin_VisitorAttributeBase_D__1__D_attribute44_row input');
+        $test_target.inputToText();
+
+        strictEqual(getTargetText($test_target), 'なし');
+    });
+
+    test('input checkbox testing - multi check -', function() {
+        var $test_target = $('#ss_admin_VisitorAttributeBase_D__1__D_attribute44_row_multicheck input');
+        $test_target.inputToText();
+
+        strictEqual(getTargetText($test_target), 'ありなし');
+    });
+
+    test('select testing', function() {
+        var $test_target = $('#ss_admin_VisitorAttributeBase_D__1__D_attribute10_row select');
+        $test_target.inputToText();
+
+        strictEqual(getTargetText($test_target), '01');
     });
 }(window.jQuery));
