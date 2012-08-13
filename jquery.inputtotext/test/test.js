f0ddc247a63f01343a3af49d1d1fa349d1374098
niwaringo <niwaringo@gmail.com>
2012-08-13 18:01:42 +0900

    add comment

--- jquery.inputtotext/test/test.js
+++ jquery.inputtotext/test/test.js
@@ -1,6 +1,24 @@
 ;(function($) {
     'use strict';
-    
-    $('#ss_admin_VisitorData_D__1__D_email_value input').val('hogehoge');
-    $('#ss_admin_VisitorData_D__1__D_email_value input').inputToText();
+
+    var class_selector = '.input_to_text',
+
+    getTargetText = function($obj) {
+        return $obj.parents('tr:first').find(class_selector).text();
+    };
+
+    test('input text testing', function() {
+        var test_value = 'testvalue',
+        $test_target = $('#ss_admin_VisitorData_D__1__D_email_value input');
+
+        $test_target.val(test_value);
+        $test_target.inputToText();
+
+        strictEqual(getTargetText($test_target), test_value);
+    });
+
+    test('input radio testing', function() {
+        var $test_target = $('#ss_admin_visitordata_d__1__d_sub_email_valid_row input'),
+        $text_target;
+    });
 }(window.jQuery));
