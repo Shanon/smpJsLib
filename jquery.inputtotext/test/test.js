32f355685c67b3babebe55667001ac7435f1c80b
niwaringo <niwaringo@gmail.com>
2012-08-13 17:31:42 +0900

    fix indent

--- jquery.inputtotext/test/test.js
+++ jquery.inputtotext/test/test.js
@@ -1,5 +1,6 @@
 ;(function($) {
     'use strict';
+    
     $('#ss_admin_VisitorData_D__1__D_email_value input').val('hogehoge');
     $('#ss_admin_VisitorData_D__1__D_email_value input').inputToText();
 }(window.jQuery));
