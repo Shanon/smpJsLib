182b7259d3f917ae8f6d17f35e8915455b36434f
niwaringo <niwaringo@gmail.com>
2012-11-05 15:51:45 +0900

    fix get.set method

--- smputl/smputl.js
+++ smputl/smputl.js
@@ -1,64 +1,171 @@
 ;(function($) {
-    'use strict';
-    var smputl = (function() {
+  'use strict';
 
-        return {
-         getValue: function($obj) {
-            var tagName, inputType,
-            checkbox_join_str = '<br />',
+  var FormObjUtl = function() {
+    this.initialize.apply(this, arguments);
+  };
 
-            getNodeTextValue = function(domobj) {
-                return domobj.nextSibling.nodeValue;
-            },
+  FormObjUtl.fn = FormObjUtl.prototype;
 
-            getRadioValue = function($obj) {
-                var $checked;
-                $checked = $obj.filter(':checked');
+  FormObjUtl.fn.initialize = function($obj, separator) {
+    this._$obj = $obj;
+    this._separator = separator || ':';
+    this._obj_info = [];
 
-                if ($checked.size() === 0) {
-                    return '';
-                }
+    this.setObjInfo();
+  };
 
-                return getNodeTextValue($checked[0]);
-            },
+  FormObjUtl.fn.setObjInfo = function() {
+    var $obj = this._$obj,
+    tag_name = this._$obj[0].tagName;
 
-            getCheckBoxValue = function($obj) {
-                var checked_strs = [],
-                $checked = $obj.filter(':checked');
+    if(tag_name === 'INPUT') {
+      return this._obj_info = [$obj, tag_name, $obj.attr('type')];
 
-                $checked.each(function() {
-                    checked_strs.push(getNodeTextValue($(this)[0]));
-                });
+    }
 
-                return checked_strs.join(checkbox_join_str);
-            };
+    if (tag_name === 'SELECT') {
+      return this._obj_info = [$obj, tag_name, tag_name];
+    }
+  };
 
-            tagName = $obj[0].tagName;
-            if (tagName === 'INPUT') {
-                inputType = $obj.attr('type');
+  FormObjUtl.fn.set = function(value, type) {
+    this.obj[this._obj_info[1]][this._obj_info[2]].set(this._obj_info[0], value, type);
+  };
 
-                if (inputType === 'text') {
-                    return $obj.val();
-                }
+  FormObjUtl.fn.get = function() {
+    return this.obj[this._obj_info[1]][this._obj_info[2]].get(this._obj_info[0], this._separator);
+  };
 
-                if (inputType === 'radio') {
-                    return getRadioValue($obj);
-                }
-                
-                if (inputType === 'checkbox') {
-                    return getCheckBoxValue($obj);
-                }
+  FormObjUtl.fn.obj = {
+    'INPUT': {
+      'text': {
+        get: function($obj) {
+          return $obj.val();
+        },
+
+        set: function ($obj, value) {
+          $obj.val(value);
+        }
+      },
+
+      'radio': {
+        get: function($obj) {
+          var $checked = $obj.filter(':checked');
+
+          if ($checked.size() === 0) {
+            return '';
+          }
+
+          return $checked[0].nextSibling.nodeValue;
+        },
+
+        set: function($obj, value, type) {
+          type = type || 'text';
+          $obj.each(function() {
+            var $this = $(this);
+
+            if (type === 'text' && $this[0].nextSibling.nodeValue.match(value)) {
+              $this.attr('checked', true);
+              return false;
             }
-            
-            if (tagName === 'SELECT') {
-                return $obj.find(':selected').text();
+
+            if (type === 'val' && $this.val() - 0 === value - 0) {
+              $this.attr('checked', true);
+              return false;
             }
-            
-            return $obj.text();
+
+          });
         }
 
-    };
-}());
+      },
 
-window.smputl = smputl;
+      'checkbox': {
+        get: function($obj, separator) {
+          var checked_strs = [],
+          $checked = $obj.filter(':checked');
+
+          $checked.each(function() {
+            checked_strs.push($(this)[0].nextSibling.nodeValue);
+          });
+
+          return checked_strs.join(separator);
+        },
+
+        set: function($obj, value, type) {
+          type = type || 'text';
+          $obj.each(function() {
+            var $this = $(this);
+
+            if (type === 'text' && $this[0].nextSibling.nodeValue.match(value)) {
+              $this.attr('checked', true);
+              return false;
+            }
+
+            if (type === 'val' && $this.val() - 0 === value - 0) {
+              $this.attr('checked', true);
+              return false;
+            }
+          });
+        }
+
+      },
+
+      'hidden': {
+        get: function($obj) { 
+          return $obj.val();
+        },
+
+        set: function($obj, value) {
+          $obj.val(value);
+        }
+      }
+    },
+
+    'SELECT': {
+      'SELECT': {
+        get: function($obj) {
+          return $obj.find(':selected').text();
+        },
+
+        set: function($obj, value, type) {
+          type = type || 'text';
+
+          if (type === 'val') {
+            $obj.val(value);
+            return;
+          }
+
+          $obj.find('option').each(function() {
+            var $this = $(this);
+
+            if ($this.text() === value) {
+              $this.attr('selected', true);
+              return false;
+            }
+          });
+        }
+      }
+    }
+
+  };
+
+
+
+  var smputl = (function() {
+
+    return {
+      setInput: function($obj, value, type) {
+        var fou = new FormObjUtl($obj);
+        fou.set(value, type);
+      },
+
+      getInput: function($obj) {
+        var fou = new FormObjUtl($obj);
+        return fou.get($obj);
+      }
+    };
+  }());
+
+  window.smputl = smputl;
 }(jQuery));
--- smputl/test/test.js
+++ smputl/test/test.js
@@ -1,33 +1,88 @@
-;(function($) {
-	'use strict';
-	var smputl = window.smputl;
-	var $input_text = $('#input_text'),
-	$select = $('#select'),
-	$radio = $('#radios input'),
-	$checkbox = $('#checkbox input');
+(function($) {
+  'use strict';
+  /*global smputl */
+  var $input_text = $('#input_text'),
+  $select = $('#select'),
+  $radio = $('#radios input'),
+  $checkbox = $('#checkbox input'),
+  $hidden = $('#hidden');
 
-	module('getValue testing', {
-		setup: function() {
-			this.text_input_val = 'text_input';
+  var text_value = 'text_value',
+  radio_value = 'radio one',
+  check_value = 'check one',
+  select_value = 'select2',
+  hidden_value = 'hidden';
 
-			$input_text.val(this.text_input_val); 
-			$('#select').find('option:eq(0)').attr('selected', true);
-			$radio.filter(':eq(0)').attr('checked', true);
+  module('setValue testing');
+  test('input testing', function() {
 
-			$checkbox.filter(':eq(0)').attr('checked', true);
-			$checkbox.filter(':eq(1)').attr('checked', true);
-		}
-	});
-	test('input text testing', function() {
-		strictEqual(smputl.getValue($input_text), this.text_input_val);
-	});
-	test('select testing', function() {
-		strictEqual(smputl.getValue($select), 'select1');
-	});
-	test('radio testing', function() {
-		strictEqual(smputl.getValue($radio), 'radio1');
-	});
-	test('checkbox testing', function() {
-		strictEqual(smputl.getValue($checkbox), 'check1<br />check2');
-	});
+    smputl.setInput($input_text, text_value);
+    strictEqual($input_text.val(), text_value, 'input text set');
+
+    smputl.setInput($radio, radio_value);
+    strictEqual($radio.val(), '1', 'input radio set');
+
+    smputl.setInput($checkbox, check_value);
+    strictEqual($checkbox.filter(':checked').val(), '1', 'input checkbox set');
+
+    smputl.setInput($hidden, hidden_value);
+    strictEqual($hidden.val(), hidden_value, 'input hidden set');
+  });
+
+  test('select testing', function() {
+    smputl.setInput($select, select_value);
+    strictEqual($select.find(':selected').text(), select_value, 'select set');
+
+    smputl.setInput($checkbox, check_value);
+    strictEqual($radio.val(), '1', 'input checkbox set');
+  });
+
+
+  test('input testing val', function() {
+    smputl.setInput($radio, 2, 'val');
+    strictEqual($radio.filter(':checked').val(), '2', 'input radio set');
+
+    $checkbox.attr('checked', false);
+    smputl.setInput($checkbox, 1, 'val');
+    strictEqual($checkbox.filter(':checked').val(), '1', 'input checkbox set');
+  });
+
+  test('select testing val', function() {
+    $('#select').val('1');
+    smputl.setInput($select, 2, 'val');
+    strictEqual($select.find(':selected').text(), select_value, 'select set');
+  });
+
+  module('getValue', {
+    setup: function() {
+      smputl.setInput($input_text, text_value);
+      smputl.setInput($radio, radio_value);
+      smputl.setInput($checkbox, check_value);
+      smputl.setInput($select, select_value);
+      smputl.setInput($hidden, hidden_value);
+    }
+  });
+  test('input testing', function() {
+    var it_value = smputl.getInput($input_text),
+    ir_value = smputl.getInput($radio),
+    ic_value = smputl.getInput($checkbox),
+    ih_value = smputl.getInput($hidden);
+
+    strictEqual(it_value, text_value, 'input text get');
+    strictEqual(ir_value, radio_value, 'input radio get');
+    strictEqual(ic_value, check_value, 'input checkbox get');
+    strictEqual(ih_value, hidden_value, 'input hidden get');
+
+    //multi check
+    smputl.setInput($checkbox, 'check two');
+    ic_value = smputl.getInput($checkbox);
+    strictEqual(ic_value, 'check one:check two', 'input multi check get');
+  });
+
+  test('select testing', function() {
+    var se_value = smputl.getInput($select);
+
+    strictEqual(se_value, select_value, 'select get');
+  });
+
 }(jQuery));
